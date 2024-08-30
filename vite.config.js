36c7d8b165d16df25fdce13aa73469e6d05dd441
SIBSANKAR MANNA <108569179+SIBSANKARMANNA@users.noreply.github.com>
2024-08-30 15:45:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<repository>/',  // replace <repository> with your actual repository name
+  base: '/shopping_nursery/',  // replace <repository> with your actual repository name
   plugins: [react()],
 });
